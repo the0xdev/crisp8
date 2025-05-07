b3727f2e48d0f560fb5eada7207d925268a28e10
Imran Mustafa <imran@imranmustafa.net>
2025-05-06 20:35:53 -0400

    basic gui

--- flake.nix
+++ flake.nix
@@ -2,42 +2,67 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+# https://gitlab.com/scvalex/sixty-two/-/blob/flake-blogpost/flake.nix
+# is the base for this flake
 {
   description = "CHIP-8 emulator";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    naersk.url = "github:nix-community/naersk";
     flake-utils.url = "github:numtide/flake-utils";
   };
   outputs = {
     self,
     nixpkgs,
+    naersk,
     flake-utils,
   }:
     flake-utils.lib.eachDefaultSystem (
       system: let
         pkgs = nixpkgs.legacyPackages.${system};
-      in
-        with pkgs; { 
+        naersk' = pkgs.callPackage naersk {};
+        libPath = with pkgs; lib.makeLibraryPath [
+          libGL
+          libxkbcommon
+          wayland
+          xorg.libX11
+          xorg.libXcursor
+          xorg.libXi
+          xorg.libXrandr
+        ];
+        pname = "crisp8";
+      in rec
+        { 
           packages = {
-            default = stdenv.mkDerivation rec {
-              name = "crisp8";
+            default = naersk'.buildPackage {
               src = ./.;
+              doCheck = true;
+              pname = pname;
 
-              RUST_BACKTRACE=1;
-              buildInputs = [
+              nativeBuildInputs = with pkgs; [
+                makeWrapper
               ];
-              nativeBuildInputs = [
-                cargo
+              buildInputs = with pkgs; [
+                xorg.libxcb
               ];
-
-              buildPhase = ''
-                cargo build -r
-              '';
-              installPhase = ''
-                mkdir -p $out/bin
-                cp target/release/${name} $out/bin
+              postInstall = ''
+                wrapProgram "$out/bin/${pname}" --prefix LD_LIBRARY_PATH : "${libPath}"
               '';
             };
+          };
+          devShell = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              cargo
+              cargo-insta
+              pre-commit
+              rust-analyzer
+              rustPackages.clippy
+              rustc
+              rustfmt
+              tokei
+              xorg.libxcb
+            ];
+            LD_LIBRARY_PATH = libPath;
           };
         }
     );
--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,9 @@ use std::fs::File;
 use std::io;
 use rand::Rng;
 
+use eframe::{
+    egui,
+};
 
 const SIZE: usize = 0x1000;
 
@@ -58,7 +61,6 @@ pub struct Crisp {
 // fn op_exe(state: Crisp, code: &[u8]) -> Result<Crisp, Error> {
 //     todo!()
 // }
-
 
 
 impl Crisp {
@@ -383,7 +385,54 @@ impl CrispBuilder {
     }
 }
 
-fn main() {
-    let c = CrispBuilder::new().load_file("test.bin").expect("File error").build();
-    c.run();
+fn main() -> eframe::Result {
+    // env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
+    let options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default().with_inner_size([320.0, 240.0]),
+        ..Default::default()
+    };
+    let mut c = CrispBuilder::new().load_file("test.bin").expect("File error").build();
+    // c.run();
+    eframe::run_native(
+        "My egui App",
+        options,
+        Box::new(|cc| {
+            // This gives us image support:
+	    
+            Ok(Box::<MyApp>::default())
+        }),
+    )
+}
+
+struct MyApp {
+    name: String,
+    age: u32,
+}
+
+impl Default for MyApp {
+    fn default() -> Self {
+        Self {
+            name: "Arthur".to_owned(),
+            age: 42,
+        }
+    }
+}
+
+impl eframe::App for MyApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("My egui Application");
+            ui.horizontal(|ui| {
+                let name_label = ui.label("Your name: ");
+                ui.text_edit_singleline(&mut self.name)
+                    .labelled_by(name_label.id);
+            });
+            ui.add(egui::Slider::new(&mut self.age, 0..=120).text("age"));
+            if ui.button("Increment").clicked() {
+                self.age += 1;
+            }
+            ui.label(format!("Hello '{}', age {}", self.name, self.age));
+
+        });
+    }
 }
